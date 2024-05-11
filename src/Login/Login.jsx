7ef5fde51ee995f0aa017250ba735e4f5d459ace
SUPTO-1 <jahidulislamsupta9@gmail.com>
2024-05-11 21:25:17 +0600

    added authentication

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -3,8 +3,68 @@ import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
+import Swal from "sweetalert2";
 
 const Login = () => {
+  const { logIn, googleSignIn, githubSignIn } = useContext(AuthContext);
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.target);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(email, password);
+    logIn(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        Swal.fire({
+          title: "success!",
+          text: "User logged in successfully",
+          icon: "success",
+          confirmButtonText: "Okay",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          title: "error!",
+          text: "Password and E-mail doesn't Match",
+          icon: "error",
+          confirmButtonText: "Okay",
+        });
+      });
+  };
+  const handleGoogle = () => {
+    googleSignIn(googleProvider)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        Swal.fire({
+          title: "success!",
+          text: "User logged in successfully",
+          icon: "success",
+          confirmButtonText: "Okay",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        Swal.fire({
+          title: "error!",
+          text: "Something went wrong!Try Again.",
+          icon: "error",
+          confirmButtonText: "Okay",
+        });
+      });
+  };
+
+
+
   return (
     <div className=" mx-2 md:mx-8 md:mt-24 font-poppins lg:flex gap-5 border-2 rounded-lg shadow-xl">
       <div className="flex-1 bg-purple-500 md:rounded-r-[150px] rounded-l-lg">
@@ -26,7 +86,7 @@ const Login = () => {
         <h2 className="text-3xl text-center font-poppins mt-4 lg:mt-0 font-bold">
           LogIn Now
         </h2>
-        <form className="card-body">
+        <form onSubmit={handleLogin} className="card-body">
           <div className="form-control w-full mx-auto">
             <label className="label">
               <span className="label-text text-xl">Email</span>
@@ -56,8 +116,8 @@ const Login = () => {
           </div>
         </form>
         <div className="flex justify-center items-center gap-5">
-          <FcGoogle className="text-3xl"></FcGoogle>
-          <FaGithub className="text-3xl"></FaGithub>
+          <FcGoogle onClick={handleGoogle} className="text-3xl"></FcGoogle>
+          <FaGithub onClick={handleGithub} className="text-3xl"></FaGithub>
           <FaFacebook className="text-3xl"></FaFacebook>
           <FaTwitter className="text-3xl"></FaTwitter>
         </div>
--- src/SignUp/SignUp.jsx
+++ src/SignUp/SignUp.jsx
@@ -1,6 +1,70 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-
+import { AuthContext } from "../Providers/AuthProvider";
+import Swal from "sweetalert2";
 const SignUp = () => {
+  const {createUser} = useContext(AuthContext);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const name = formData.get("name");
+    const photo = formData.get("photo");
+    // password validation here
+    const passwordCheckerUpper = /^(?=.*[A-Z]).*$/;
+    const passwordCheckerLower = /^(?=.*[a-z]).*$/;
+
+    if (password.length < 6) {
+      Swal.fire({
+        title: "error!",
+        text: "Password Length Must be more then 6",
+        icon: "error",
+        confirmButtonText: "Okay",
+      });
+      return;
+    }
+    else if (!passwordCheckerUpper.test(password)) {
+      Swal.fire({
+        title: "error!",
+        text: "Password must contain at least one uppercase letter",
+        icon: "error",
+        confirmButtonText: "Okay",
+      });
+      return;
+    }
+    else if (!passwordCheckerLower.test(password)) {
+      Swal.fire({
+        title: "error!",
+        text: "Password must contain at least one lowercase letter",
+        icon: "error",
+        confirmButtonText: "Okay",
+      });
+      return;
+    }
+
+    //Authentication here
+    createUser(email, password, name, photo)
+      .then(result => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        Swal.fire({
+          title: "success!",
+          text: "User SignUp successfully",
+          icon: "success",
+          confirmButtonText: "Okay",
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        Swal.fire({
+          title: "error!",
+          text: "Something went wrong. Try again!",
+          icon: "error",
+          confirmButtonText: "Okay",
+        });
+      })
+  }
     return (
         <div className="mx-2 md:mx-8 font-roboto mt-24 lg:flex flex-row-reverse gap-5 border-2 rounded-lg shadow-xl">
         <div className="flex-1 bg-purple-500 md:rounded-l-[150px] rounded-r-lg ">
@@ -22,7 +86,7 @@ const SignUp = () => {
           <h2 className="text-3xl text-center font-poppins mt-4 lg:mt-0 font-bold">
             Register Now
           </h2>
-          <form className="card-body">
+          <form onSubmit={handleRegister} className="card-body">
             <div className="form-control w-full mx-auto">
               <label className="label">
                 <span className="label-text text-xl">Name</span>
